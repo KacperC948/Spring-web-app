345a044871a475c5291d000ffcc308037201c49d
KacperC948 <miloord899@gmail.com>
2022-11-07 20:37:38 +0100

    Add hint for repository

--- src/main/java/guru/springframework/spring5webapp/repositories/BookRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/BookRepository.java
@@ -3,5 +3,11 @@ package guru.springframework.spring5webapp.repositories;
 import guru.springframework.spring5webapp.domain.Book;
 import org.springframework.data.repository.CrudRepository;
 
+//Spring Data tworzy repozytorium dla encji Book dzieki, ktorej po rozszerzeniu interfejsu Crud
+// Repository bedziemy mogli w kontrolerze wykorzystac wszyskie metody crudowych takie jak save, find...
+// Jako parametr w dla interfejsu CrudRepository podajemy jako pierwszy dla jakiej encji chcemy sworzyc
+// repozytorium oraz jako drugi typ klucza głównego tej encji
+//
+//Repozytorium jest to obiekt dzieki któremu wykonujemy operacje bezpośrednio na bazie danych
 public interface BookRepository extends CrudRepository<Book, Long> {
 }
